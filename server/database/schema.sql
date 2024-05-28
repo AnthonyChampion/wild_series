6912ff4555389dff9c6fae5a804717f8682b2177
ANTHONY CHAMPION <anthonyrodrigues.c@gmail.com>
2024-05-28 17:10:11 +0200

    quest 5 done

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,12 +1,28 @@
 create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+    id int unsigned primary key auto_increment not null,
+    email varchar(255) not null unique,
+    password varchar(255) not null
 );
 
 create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+    id int unsigned primary key auto_increment not null,
+    title varchar(255) not null,
+    user_id int unsigned not null,
+    foreign key (user_id) references user (id)
+);
+
+CREATE TABLE category (
+    id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE program (
+    id INT PRIMARY KEY auto_increment not null,
+    title VARCHAR(255) NOT NULL,
+    synopsis TEXT NOT NULL,
+    poster TEXT,
+    country VARCHAR(100) NOT NULL,
+    year INT NOT NULL,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES category (id)
 );
