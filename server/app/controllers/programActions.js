ae9b63ae056b9482bd8a369ae2a81eab68ebbd3d
ANTHONY CHAMPION <anthonyrodrigues.c@gmail.com>
2024-05-29 10:10:13 +0200

    Quest 6 done

--- server/app/controllers/programActions.js
+++ server/app/controllers/programActions.js
@@ -1,4 +1,4 @@
-// Some data to make the trick
+const tables = require("../../database/tables");
 
 const programs = [
     {
@@ -25,10 +25,24 @@ const programs = [
 
 // Declare the action
 
-const browse = (req, res) => {
-    res.json(programs);
+const browse = async (req, res) => {
+    const programsFromDB = await tables.program.readAll();
+
+    res.json(programsFromDB);
+};
+
+const read = (req, res) => {
+    const parsedId = parseInt(req.params.id, 10);
+
+    const program = programs.find((p) => p.id === parsedId);
+
+    if (program != null) {
+        res.json(program);
+    } else {
+        res.sendStatus(404);
+    }
 };
 
 // Export it to import it somewhere else
 
-module.exports = { browse };
+module.exports = { browse, read };
